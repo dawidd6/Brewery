3cf8d5e49e75c16d547e8d6da494fe278283fce5
Dawid Dziurla <dawidd0811@gmail.com>
2021-02-13 17:06:44 +0100

    lib: update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Brewery",
       theme: ThemeData(
         primarySwatch: MyColors.brownMaterial,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: MyColors.brown,
       ),
       home: Home(),
     );
@@ -21,7 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatefulWidget {
-  var title = "Brewery";
+  final title = "Brewery";
 
   Home({Key key}) : super(key: key);
 
@@ -35,9 +37,6 @@ class HomeState extends State<Home> {
     var response = await http.get(url);
     var obj = convert.jsonDecode(response.body);
     var fullName = obj["full_name"];
-    setState(() {
-      widget.title = fullName;
-    });
   }
 
   @override
@@ -51,20 +50,20 @@ class HomeState extends State<Home> {
 }
 
 class MyColors {
-  static const int brown = 0xFF2F2A25;
-  static const MaterialColor brownMaterial = MaterialColor(
-    brown,
+  static final Color brown = Color(0xFF2F2A25);
+  static final MaterialColor brownMaterial = MaterialColor(
+    brown.value,
     <int, Color>{
-      50: Color(brown),
-      100: Color(brown),
-      200: Color(brown),
-      300: Color(brown),
-      400: Color(brown),
-      500: Color(brown),
-      600: Color(brown),
-      700: Color(brown),
-      800: Color(brown),
-      900: Color(brown),
+      50: brown,
+      100: brown,
+      200: brown,
+      300: brown,
+      400: brown,
+      500: brown,
+      600: brown,
+      700: brown,
+      800: brown,
+      900: brown,
     },
   );
 }
