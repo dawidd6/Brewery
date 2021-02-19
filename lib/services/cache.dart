2274c9266825ab761d3a4be12fe4223771c999a1
Dawid Dziurla <dawidd0811@gmail.com>
2021-02-19 01:58:25 +0100

    cache: add some methods

--- lib/services/cache.dart
+++ lib/services/cache.dart
@@ -1,10 +1,44 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:brewery/models/cask.dart';
+import 'package:brewery/models/formula.dart';
+import 'package:brewery/services/api.dart';
 import 'package:path_provider/path_provider.dart';
 
 class Cache {
-  getCacheDir() async {
-    Directory tempDir = await getTemporaryDirectory();
-    String tempPath = tempDir.path;
+  Future<File> writeCasks(List<Cask> casks) async {
+    var cacheDirPath = await getCacheDirPath();
+    var filePath = cacheDirPath + API.caskEndpoint;
+    var file = File(filePath);
+    return file.writeAsString(jsonEncode(casks));
+  }
+
+  Future<File> writeFormulae(List<Formula> formulae) async {
+    var cacheDirPath = await getCacheDirPath();
+    var filePath = cacheDirPath + API.formulaEndpoint;
+    var file = File(filePath);
+    return file.writeAsString(jsonEncode(formulae));
+  }
+
+  Future<List<Cask>> readCasks() async {
+    var cacheDirPath = await getCacheDirPath();
+    var filePath = cacheDirPath + API.caskEndpoint;
+    var file = File(filePath);
+    var contents = await file.readAsString();
+    return jsonDecode(contents);
+  }
+
+  Future<List<Formula>> readFormulae() async {
+    var cacheDirPath = await getCacheDirPath();
+    var filePath = cacheDirPath + API.formulaEndpoint;
+    var file = File(filePath);
+    var contents = await file.readAsString();
+    return jsonDecode(contents);
+  }
+
+  Future<String> getCacheDirPath() async {
+    var dir = await getTemporaryDirectory();
+    return dir.path;
   }
 }
