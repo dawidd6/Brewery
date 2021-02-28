67371549bca0bc0668d34d786a7f49a573d3d748
Dawid Dziurla <dawidd0811@gmail.com>
2021-02-28 01:42:56 +0100

    services: use cache

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:brewery/models/cask.dart';
 import 'package:brewery/models/formula.dart';
+import 'package:brewery/services/cache_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
@@ -29,28 +30,33 @@ class ApiService {
     );
   }
 
-  static Future<List<Formula>> fetchFormulae() async {
-    print("Fetching formulae");
-
-    final response = await http.get(baseURL + formulaeEndpoint);
-    //final responseLinux = await http.get(baseURL + formulaLinuxEndpoint);
-
-    if (response.statusCode == 200) {
-      return compute(parseFormulae, response.body);
+  static Future<List<dynamic>> _fetchObjects(
+      Function parseFunction, String endpoint) async {
+    try {
+      print("$endpoint: trying cache");
+      final data = await CacheService.read(endpoint);
+      print("$endpoint: cache hit");
+      return compute(parseFunction, data);
+    } catch (exception) {
+      print("$endpoint: calling api");
+      final response = await http.get(baseURL + endpoint);
+      print("$endpoint: got response from api");
+      if (response.statusCode == 200) {
+        print("$endpoint: saving cache");
+        await CacheService.write(response.body, endpoint);
+        print("$endpoint: cache saved");
+        return compute(parseFunction, response.body);
+      }
     }
 
-    throw Exception("Failed to load formulae");
+    throw Exception();
+  }
+
+  static Future<List<Formula>> fetchFormulae() async {
+    return await _fetchObjects(parseFormulae, formulaeEndpoint);
   }
 
   static Future<List<Cask>> fetchCasks() async {
-    print("Fetching casks");
-
-    final response = await http.get(baseURL + casksEndpoint);
-
-    if (response.statusCode == 200) {
-      return compute(parseCasks, response.body);
-    }
-
-    throw Exception("Failed to load casks");
+    return await _fetchObjects(parseCasks, casksEndpoint);
   }
 }
--- lib/services/cache_service.dart
+++ lib/services/cache_service.dart
@@ -1,44 +1,26 @@
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:brewery/models/cask.dart';
-import 'package:brewery/models/formula.dart';
-import 'package:brewery/services/api_service.dart';
 import 'package:path_provider/path_provider.dart';
 
+class CacheTooOldException implements Exception {
+  CacheTooOldException();
+}
+
 class CacheService {
-  Future<File> writeCasks(List<Cask> casks) async {
-    var cacheDirPath = await getCacheDirPath();
-    var filePath = cacheDirPath + ApiService.casksEndpoint;
+  static Future write(String data, String endpoint) async {
+    var cacheDirPath = await getTemporaryDirectory();
+    var filePath = cacheDirPath.path + endpoint;
     var file = File(filePath);
-    return file.writeAsString(jsonEncode(casks));
+    return file.writeAsString(data);
   }
 
-  Future<File> writeFormulae(List<Formula> formulae) async {
-    var cacheDirPath = await getCacheDirPath();
-    var filePath = cacheDirPath + ApiService.formulaeEndpoint;
+  static Future<String> read(String endpoint) async {
+    var cacheDirPath = await getTemporaryDirectory();
+    var filePath = cacheDirPath.path + endpoint;
     var file = File(filePath);
-    return file.writeAsString(jsonEncode(formulae));
-  }
-
-  Future<List<Cask>> readCasks() async {
-    var cacheDirPath = await getCacheDirPath();
-    var filePath = cacheDirPath + ApiService.casksEndpoint;
-    var file = File(filePath);
-    var contents = await file.readAsString();
-    return jsonDecode(contents);
-  }
-
-  Future<List<Formula>> readFormulae() async {
-    var cacheDirPath = await getCacheDirPath();
-    var filePath = cacheDirPath + ApiService.formulaeEndpoint;
-    var file = File(filePath);
-    var contents = await file.readAsString();
-    return jsonDecode(contents);
-  }
-
-  Future<String> getCacheDirPath() async {
-    var dir = await getTemporaryDirectory();
-    return dir.path;
+    var mod = file.lastModifiedSync();
+    if (mod.difference(DateTime.now()) > Duration(minutes: 10))
+      throw CacheTooOldException();
+    return file.readAsString();
   }
 }
