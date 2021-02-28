19a109f9769801f178154855bb0b3cbad0b72d3c
Dawid Dziurla <dawidd0811@gmail.com>
2021-02-28 03:37:50 +0100

    widgets: better failure text

--- lib/widgets/failure_text.dart
+++ lib/widgets/failure_text.dart
@@ -2,14 +2,30 @@ import 'package:flutter/material.dart';
 
 class FailureText extends StatelessWidget {
   final String message;
+  final Function onRefresh;
 
-  FailureText({Key key, @required this.message}) : super(key: key);
+  FailureText({Key key, @required this.message, this.onRefresh})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      message,
-      style: Theme.of(context).textTheme.bodyText1,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          message,
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        IconButton(
+          icon: Icon(Icons.refresh),
+          iconSize: 64.0,
+          onPressed: onRefresh,
+        ),
+        Text(
+          "",
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ],
     );
   }
 }
