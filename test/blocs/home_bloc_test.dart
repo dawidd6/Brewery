3468c5e74592c92bcc63ac1781fc21ef4778636e
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-04 21:28:46 +0100

    test: fix

--- test/blocs/home_bloc_test.dart
+++ test/blocs/home_bloc_test.dart
@@ -1,14 +1,11 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:brewery/blocs/home_bloc.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('HomeBloc', () {
-    blocTest(
-      'change',
-      build: () => HomeBloc(),
-      act: (HomeBloc bloc) => bloc.change(1),
-      expect: () => [1],
-    );
-  });
+  blocTest(
+    'change page',
+    build: () => HomeBloc(),
+    act: (HomeBloc bloc) => bloc.add(HomeChangePageEvent(1)),
+    expect: () => [HomePageState(1)],
+  );
 }
