943e3c57699c9e1adc6ef5086be806f74d4fa246
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-05 11:18:19 +0100

    regexp_filter: add cancel suffix icon

--- lib/widgets/regexp_filter.dart
+++ lib/widgets/regexp_filter.dart
@@ -4,24 +4,31 @@ class RegexpFilter extends StatelessWidget {
   final void Function(String) onChanged;
   final int filteredCount;
   final int totalCount;
+  final TextEditingController controller;
 
   RegexpFilter({
     Key? key,
     required this.onChanged,
     required this.filteredCount,
     required this.totalCount,
-  });
+  }) : controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: TextField(
+        controller: controller,
         onChanged: onChanged,
         decoration: InputDecoration(
           prefixIcon: Icon(
             Icons.search,
             color: Theme.of(context).inputDecorationTheme.labelStyle!.color,
+          ),
+          suffixIcon: controller.text.isEmpty ? null : IconButton(
+            icon: Icons.cancel,
+            color: Theme.of(context).inputDecorationTheme.labelStyle!.color,
+            onPressed: controller.clear,
           ),
           labelText: 'Search',
           hintText: 'hello',
