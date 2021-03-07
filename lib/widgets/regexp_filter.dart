c2cc84a12561a581dad307d611cc7af3a7be0445
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-08 00:32:47 +0100

    regexp_filter: make it stateful

--- lib/widgets/regexp_filter.dart
+++ lib/widgets/regexp_filter.dart
@@ -1,43 +1,62 @@
+import 'package:brewery/widgets/conditional_widget.dart';
 import 'package:flutter/material.dart';
 
-class RegexpFilter extends StatelessWidget {
+class RegexpFilter extends StatefulWidget {
   final void Function(String) onChanged;
-  final TextEditingController controller;
   final String title;
 
   RegexpFilter({
     Key? key,
     required this.title,
     required this.onChanged,
-    required this.controller,
   });
+
+  @override
+  _RegexpFilterState createState() => _RegexpFilterState();
+}
+
+class _RegexpFilterState extends State<RegexpFilter> {
+  final _controller = TextEditingController();
+
+  void onClear() {
+    _controller.clear();
+    widget.onChanged('');
+  }
+
+  void onInput(String input) {
+    widget.onChanged(input);
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: controller,
-      onChanged: onChanged,
+      controller: _controller,
+      onChanged: onInput,
       decoration: InputDecoration(
+        labelText: widget.title,
         prefixIcon: Icon(
           Icons.search,
           size: 24.0,
           color: Theme.of(context).inputDecorationTheme.labelStyle!.color,
         ),
-        suffixIcon: controller.text.isEmpty
-            ? null
-            : IconButton(
-                onPressed: () {
-                  controller.clear();
-                  onChanged('');
-                },
-                icon: Icon(
-                  Icons.clear,
-                  size: 24.0,
-                  color:
-                      Theme.of(context).inputDecorationTheme.labelStyle!.color,
-                ),
-              ),
-        labelText: title,
+        suffixIcon: ConditionalWidget.nullable(
+          condition: _controller.text.isNotEmpty,
+          widgetIfTrue: IconButton(
+            onPressed: onClear,
+            icon: Icon(
+              Icons.clear,
+              size: 24.0,
+              color: Theme.of(context).inputDecorationTheme.labelStyle!.color,
+            ),
+          ),
+        ),
       ),
     );
   }
