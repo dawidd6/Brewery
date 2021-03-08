d05df76d0fc70d1f3383cda081fa6c04db4667fb
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-09 00:58:35 +0100

    test: clean

--- test/blocs/home_bloc_test.dart
+++ test/blocs/home_bloc_test.dart
@@ -1,11 +1,1 @@
-import 'package:bloc_test/bloc_test.dart';
-import 'package:brewery/blocs/home/home_bloc.dart';
-
-void main() {
-  blocTest(
-    'change page',
-    build: () => HomeBloc(),
-    act: (HomeBloc bloc) => bloc.add(HomeChangePageEvent(1)),
-    expect: () => [1],
-  );
-}
+void main() {}
