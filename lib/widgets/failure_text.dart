61b1ba4d277fb6505d7ad98ba9ffc0e1a5b6162a
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-10 02:40:46 +0100

    failure_text: no refresh

--- lib/widgets/failure_text.dart
+++ lib/widgets/failure_text.dart
@@ -2,34 +2,18 @@ import 'package:flutter/material.dart';
 
 class FailureText extends StatelessWidget {
   final String message;
-  final void Function() onRefresh;
 
   FailureText({
     Key? key,
     required this.message,
-    required this.onRefresh,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: () async {
-        onRefresh();
-        return null;
-      },
-      child: LayoutBuilder(
-        builder: (context, constraints) => SingleChildScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          child: SizedBox(
-            height: constraints.maxHeight,
-            child: Center(
-              child: Text(
-                message,
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            ),
-          ),
-        ),
+    return Center(
+      child: Text(
+        message,
+        style: Theme.of(context).textTheme.bodyText1,
       ),
     );
   }
