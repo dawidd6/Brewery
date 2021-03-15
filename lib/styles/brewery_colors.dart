79534b316d270dbe4c1aa0b1807a5548b064389e
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-15 17:17:44 +0100

    colors: update

--- lib/styles/brewery_colors.dart
+++ lib/styles/brewery_colors.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class BreweryColors {
   static final Color brown = Color(0xFF2F2A25);
-  static final Color brownDark = Color(0xFF211D19);
+  static final Color brownDark = Color(0xFF26221D);
+  static final Color brownDarker = Color(0xFF211D19);
+  static final Color brownDarkest = Color(0xFF1C1916);
   static final Color goldLight = Color(0xFFFFCE9B);
   static final Color goldDark = Color(0xFFC5843D);
 }
