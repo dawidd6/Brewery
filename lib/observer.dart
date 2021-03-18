786ccc7664731854bcf0cb05f5460b08ca8c6916
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-18 19:15:05 +0100

    observer: handle changes

--- lib/observer.dart
+++ lib/observer.dart
@@ -2,9 +2,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyBlocObserver extends BlocObserver {
   @override
-  void onCreate(Bloc bloc) {
+  void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
     print('onCreate -- bloc: ${bloc.runtimeType}');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('onChange -- bloc: ${bloc.runtimeType}, change: $change');
   }
 
   @override
@@ -20,13 +26,13 @@ class MyBlocObserver extends BlocObserver {
   }
 
   @override
-  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     print('onError -- bloc: ${bloc.runtimeType}, error: $error');
     super.onError(bloc, error, stackTrace);
   }
 
   @override
-  void onClose(Bloc bloc) {
+  void onClose(BlocBase bloc) {
     super.onClose(bloc);
     print('onClose -- bloc: ${bloc.runtimeType}');
   }
